5899437f2edd938b2af4c6800b0fa0a2804111b3
Natalia Palej <palejnatalia@gmail.com>
2022-02-08 10:47:58 +0000

    Lab5 Continuing methods with arrays

--- Lab5/src/Lab5_Test.java
+++ Lab5/src/Lab5_Test.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Lab5_Test 
 {
@@ -11,6 +12,41 @@ public class Lab5_Test
 		
 		Arrays.sort(a);
 		System.out.println("a: " + Arrays.toString(a));
+		
+		System.out.println();
+		
+		double[] b = new double[10];
+		MyArrayMethods.fillRandom(b, 1, 10);
+		System.out.println("b: " + Arrays.toString(b));
+		Arrays.sort(b);
+		System.out.println("b: " + Arrays.toString(b));
+		
+		System.out.println();
+		
+		//create second array and partially fill
+		Scanner in = new Scanner(System.in);
+		int[] c = new int[10];
+		int currentSize = MyArrayMethods.fillPartial(c);
+		System.out.println("c: " + Arrays.toString(c));
+		Arrays.sort(c, 0, currentSize);
+		System.out.println("c: " + Arrays.toString(c));
+		
+		System.out.println();
+		
+		//create second array and partially fill with doubles
+		double[] d = new double[10];
+		double currentSizeDouble = MyArrayMethods.fillPartial(d);
+		System.out.println("d: " + Arrays.toString(d));
+		
+		System.out.println("d: " + Arrays.toString(d));
+		
+		System.out.println(); 
+		
+		int[][] e = new int[4][4];
+		MyArrayMethods.fillRandom(e, 1, 10);
+		System.out.println("e: " + Arrays.toString(e));
+		
+		
 	}
 
 }
--- Lab5/src/MyArrayMethods.java
+++ Lab5/src/MyArrayMethods.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MyArrayMethods 
 {
 	/* Fills an integer array with values between the ranges Min and Max
@@ -9,16 +12,74 @@ public class MyArrayMethods
 	{
 		for (int i = 0; i < values.length; i++)
 		{
-			int num = (int) ((Math.random() * rangeMax)+1);
-
-			while (num < rangeMin)
+			values[i] = (int)(Math.random() * (rangeMax - rangeMin + 1)) + rangeMin;
+			if (values[i] > rangeMax)
 			{
-				num = (int) ((Math.random() * rangeMax)+1);
-			} 
-			
-			values[i] = num;
-			//System.out.println(i + " : " + num); 
+				values[i] = rangeMax;
+			}
 		}		
 	}
-
+	
+	public static void fillRandom(double[] values, double rangeMin, double rangeMax)
+	{
+		for (int i = 0; i < values.length; i++)
+		{
+			values[i] = (double) ((Math.random() * rangeMax - rangeMin)+ rangeMin);
+		/*	if (values[i] > rangeMax)
+			{
+				values[i] = rangeMax;
+			}
+		*/	
+		}		
+	}
+	
+	/* Fills an integer array from the keyboard; the array can be partially filled
+	 * The user is informed if the array is full and input should be stopped
+	 * @param values The array to be filled
+	 * @return the currentSize of the partially filled array */
+	 
+	
+	public static int fillPartial(int[] values)
+	{
+		int currentSize = 0;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter values: ");
+		while (in.hasNextInt() && currentSize < values.length)
+		{
+			values[currentSize] = in.nextInt();
+			currentSize++;
+		}
+		//in.close();
+		return currentSize;
+	}
+	
+	public static double fillPartial(double[] values)
+	{
+		int currentSizeDouble = 0;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter double values: ");
+		while (in.hasNextDouble() && currentSizeDouble < values.length)
+		{
+			values[currentSizeDouble] = in.nextDouble();
+			currentSizeDouble++;
+		}
+		//in.close();
+		return currentSizeDouble;
+	}
+	
+	//Q5 create an integer array first
+	public static void fillRandom(int[][] values, int rangeMin, int rangeMax)
+	{
+		for (int i = 0; i < values.length; i++)
+		{ 
+			System.out.print(values[i]);
+			/*for (int j = 0; j < values.length; j++)
+			{
+				System.out.print(values[i][j]);
+			}
+		}	
+		System.out.print("j[" + j + "] " + values[i][j]);*/
+	}
+	
 }
+ 
